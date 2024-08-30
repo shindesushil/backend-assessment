1b77f4fc3d77ad37aeb8013ad2741c64bc8adce7
Sushil <sushilshinde9158@gmail.com>
2024-08-30 17:28:41 +0530

    Minor fix

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const client = new Client({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
-  password: "",
+  password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
 });
 
